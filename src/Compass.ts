73ec6250bce2b104d57ad6118c3e92859659b364
Simone Ceccarelli <ceccarellisimone1@gmail.com>
2022-05-16 11:58:38 +0200

    added pan

--- src/Compass.ts
+++ src/Compass.ts
@@ -3,6 +3,10 @@ class Compass {
   private camera: THREE.PerspectiveCamera;
   private renderer: THREE.WebGLRenderer;
   private zoomOptions: { minZoom: number; maxZoom: number; step: number };
+  private draggingObj: {
+    isItDragging: boolean;
+    positions: { x: number; y: number };
+  };
 
   constructor(camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer) {
     this.camera = camera;
@@ -12,11 +16,45 @@ class Compass {
       minZoom: 10000,
       step: 0.06,
     };
+    this.draggingObj = { isItDragging: false, positions: { x: 0, y: 0 } };
     this.setAllEvents();
   }
 
   setAllEvents = () => {
     this.setZoomHandler();
+    this.setDragCanvas();
+  };
+
+  setDragCanvas = () => {
+    this.renderer.domElement.onmousedown = (event) => {
+      const { clientX, clientY } = event;
+      this.draggingObj.isItDragging = true;
+      const { x, y } = this.getWorldToCanvasABSCoordinates(clientX, clientY);
+      this.draggingObj.positions = { x, y };
+    };
+
+    window.onmousemove = (event) => {
+      if (this.draggingObj.isItDragging) {
+        const { clientX, clientY } = event;
+        const { x, y } = this.draggingObj.positions;
+        const { x: newX, y: newY } = this.getWorldToCanvasABSCoordinates(
+          clientX,
+          clientY
+        );
+        this.draggingObj.positions = { x: newX, y: newY };
+        this.camera.translateX(x - newX);
+        this.camera.translateY(y - newY);
+      }
+    };
+
+    this.renderer.domElement.onmouseup = (event) => {
+      this.draggingObj.isItDragging = false;
+    };
+  };
+
+  getWorldToCanvasABSCoordinates = (wx: number, wy: number) => {
+    const { z } = this.camera.position;
+    return this.getWorldToCanvasABSWithCustomZ(wx, wy, z);
   };
 
   setZoomHandler = () => {
@@ -25,19 +63,45 @@ class Compass {
       this.zoom(event);
     };
   };
+
   getWorldToCanvasPercentCoordinates = (wx: number, wy: number) => {
     const canvas = this.renderer.domElement;
     const percentX = (wx / canvas.clientWidth) * 2 - 1;
     const percentY = -(wy / canvas.clientHeight) * 2 + 1;
     return { percentX, percentY };
   };
+
   getABSBounds = () => {
     return this.getABSBoundsWithCustomZ(this.camera.position.z);
   };
+
   getWorldToCanvasCoordinates = (wx: number, wy: number) => {
     const { z } = this.camera.position;
     return this.getWorldToCanvasWithCustomZ(wx, wy, z);
   };
+
+  getWorldToCanvasABSPercentCoordinates = (wx: number, wy: number) => {
+    const canvas = this.renderer.domElement;
+    const percentX = wx / canvas.clientWidth;
+    const percentY = 1 - wy / canvas.clientHeight;
+    return { percentX, percentY };
+  };
+
+  private getWorldToCanvasABSWithCustomZ = (
+    wx: number,
+    wy: number,
+    cz: number
+  ) => {
+    const { percentX, percentY } = this.getWorldToCanvasABSPercentCoordinates(
+      wx,
+      wy
+    );
+    const { width, height } = this.getABSBoundsWithCustomZ(cz);
+    const retX = width * percentX;
+    const retY = height * percentY;
+    return { x: retX, y: retY };
+  };
+
   private getWorldToCanvasWithCustomZ = (
     wx: number,
     wy: number,
@@ -53,6 +117,7 @@ class Compass {
     const retY = y + (height / 2) * percentY;
     return { x: retX, y: retY };
   };
+
   private getABSBoundsWithCustomZ = (z: number) => {
     const fov = this.camera.fov;
     const aspect = this.camera.aspect;
@@ -60,6 +125,7 @@ class Compass {
     const halfWidth = halfHeight * aspect;
     return { width: halfWidth * 2, height: halfHeight * 2 };
   };
+
   private zoom = (event: WheelEvent) => {
     const { clientX, clientY, deltaY } = event;
     const { step, minZoom, maxZoom } = this.zoomOptions;
